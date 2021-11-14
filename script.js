c17395ab40abec5d49e4fc3bce5ad33e24d30727
Andrew McInally <realfunnyestate@yahoo.com>
2021-11-14 11:14:30 -0700

    updated

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.addEventListener("keydown", function (e) {
-  console.log(e);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+
+  if (!audio) {
+    //STOP the function from running
+    return;
+  }
 });
